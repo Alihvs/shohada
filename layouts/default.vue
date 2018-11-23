e07e6c85f95d84c0c9794ecb6ab8c4e4e9a7cce0
alihvs@gmail.com <alihvs@gmail.com>
2018-11-23 21:50:36 +0330

    Vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app>
+        <the-header/>
+        <v-content :ma-0="$vuetify.breakpoint.mdAndDown">
+            <nuxt/>
+        </v-content>
+        <the-footer/>
+    </v-app>
+</template>
+
+<script>
+import TheHeader from '~/components/layouts/header';
+import TheFooter from '~/components/layouts/footer';
+export default {
+    components: {
+        TheHeader,
+        TheFooter,
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>Working as intended, No?</div>
+    <section class="wrapper">
+        <v-btn flat color="cyan">Click this</v-btn>
+        <v-btn depressed dark color="pink">
+            <v-icon left>settings_remote</v-icon>
+            <span>Remote Things</span>
+        </v-btn>
+    </section>
 </template>
 
 <script>
+
+
 export default {
-    
+
 }
 </script>
 
 <style lang="scss" scoped>
-
+.wrapper {
+}
 </style>
 
 
