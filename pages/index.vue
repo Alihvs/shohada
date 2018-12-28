c04ba6c4df11f3a0b672ba3fb1c06222b190eeef
Ali Havasi <alihavasi@admins-MacBook-Pro.local>
2018-12-28 15:59:52 +0330

    second commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,15 @@
 <template>
-    <section class="wrapper">
-        <v-btn flat color="cyan">Click this</v-btn>
-        <v-btn depressed dark color="pink">
-            <v-icon left>settings_remote</v-icon>
-            <span>Remote Things</span>
-        </v-btn>
-    </section>
+    <div>Working as intended, No?</div>
 </template>
 
 <script>
-
-
 export default {
-
+    
 }
 </script>
 
 <style lang="scss" scoped>
-.wrapper {
-}
+
 </style>
 
 
