1cafd88ed54d6821d5cd7721899acbbc0ac2634a
Ali Havasi <alihavasi@admins-MacBook-Pro.local>
2019-03-20 17:03:57 +0330

    feat: add new skeleton

---
+++ components/layouts/nav/index.vue
@@ -0,0 +1,80 @@
+<template>
+    <nav class="nav-wrapper">
+        <ul class="nav-list">
+            <nav-item
+                v-for="(item, index) in links"
+                :key="index"
+                :label="item.label"
+                :component="item.component"
+            />
+        </ul>
+        <search-component/>
+    </nav>
+</template>
+
+<script>
+import NavItem from "./nav-item";
+import SearchComponent from "./search-component";
+export default {
+    components: {
+        NavItem,
+        SearchComponent
+    },
+    data() {
+        return {
+            links: [
+                {
+                    component: "home",
+                    label: "صفحه اصلی"
+                },
+                {
+                    component: "news",
+                    label: "آخرین اخبار"
+                },
+                {
+                    component: "video-news",
+                    label: "خبرهای تصویری"
+                },
+                {
+                    component: "daily-photo",
+                    label: "عکس روز"
+                },
+                {
+                    component: "video",
+                    label: "ویديو"
+                },
+                {
+                    component: "shohada",
+                    label: "گنجینه شهدا"
+                },
+                {
+                    component: "contact-us",
+                    label: "ارتباط با ما"
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/shared.scss";
+.nav-wrapper {
+    height: 30px;
+    font-size: 1rem;
+    // background-color: $primary-purple;
+    // color: $primary-white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .nav-list {
+        display: flex;
+        // border: 1px solid red;
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
+
+
---
+++ components/layouts/nav/nav-item.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="item-wrapper">{{ label }}</li>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        component: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/shared.scss";
+.item-wrapper {
+    font-size: 1.5rem;
+    width: 11rem;
+    @include flex-center();
+    &:not(:last-child) {
+        border-left: 1px solid $grey-2;
+    }
+}
+</style>
+
