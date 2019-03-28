0b42d1ff68ca373edabeaa7e67b5e3988847ad6a
Ali Havasi <alihavasi@admins-MacBook-Pro.local>
2019-03-28 16:11:16 +0430

    contact page

---
+++ components/form-inputs/text-area.vue
@@ -0,0 +1,26 @@
+<template>
+    <textarea class="text-area" name id cols="30" rows="10" :placeholder="placeholder"></textarea>
+</template>
+
+<script>
+export default {
+    props: {
+        type: String,
+        default: ""
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/shared.scss";
+.text-area {
+    border: none;
+    outline: none;
+    background-color: $grey-2;
+    padding: $m-10;
+    &::placeholder {
+        color: $grey-3;
+    }
+}
+</style>
+
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,44 @@
 <template>
-    <div>contact</div>
+    <main class="contact-wrapper">
+        <h1 class="title">ارتباط با ما</h1>
+        <article class="form">
+            <text-input placeholder="نام و نام خانوادگی"/>
+            <text-input placeholder="شماره تماس"/>
+            <text-input placeholder="پست الکترونیک"/>
+            <text-input placeholder="شماره تلفن همراه"/>
+            <text-area placeholder="متن پیام" class="text-area-comp"/>
+        </article>
+    </main>
 </template>
 
 <script>
-export default {};
+import TextInput from "~/components/form-inputs/text-input";
+import TextArea from "~/components/form-inputs/text-area";
+export default {
+    components: {
+        TextInput,
+        TextArea
+    }
+};
 </script>
+
+<style lang="scss" scoped>
+@import "~/assets/css/shared.scss";
+.contact-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
+    .title {
+        grid-column: 1 / -1;
+    }
+    .form {
+        display: grid;
+        grid-gap: 1rem;
+        grid-template-columns: 1fr 1fr;
+        .text-area-comp {
+            grid-column: 1 / -1;
+        }
+    }
+}
+</style>
+
